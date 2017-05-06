82ad2b5b1f5321551a0e788abcb29da15135bed7
陈鹏杰 <260083304@qq.com>
2017-05-06 22:00:39 +0800

    添加HTTP协议

--- PHP/APP_interface/api.php
+++ PHP/APP_interface/api.php
@@ -21,3 +21,22 @@ abstract class Api {
 
 	abstract function response($code, $message, $data);
 }
+		abstract class Api {
+
+		const JSON = 'Json';
+		const XML = 'Xml';
+		const ARR = 'Array';
+
+		/**
+		 * 定义工厂方法
+		 * param string $type 返回数据类型
+		 */
+		public static function factory($type = self::JSON) {
+			$type = isset($_GET['format']) ? $_GET['format'] : $type;
+			$resultClass = ucwords($type);
+			require_once('./Response/' . $type . '.php');
+			return new $resultClass();
+		}
+
+		abstract function response($code, $message, $data);
+	}
