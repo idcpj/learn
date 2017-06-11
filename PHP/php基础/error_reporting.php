fd256f44540d6c3f53d728eb1714f69862f22f2f
idcpj <260083304@qq.com>
2017-06-11 14:58:33 +0800

    学习更新

---
+++ PHP/php基础/error_reporting.php
@@ -0,0 +1,7 @@
+<?php
+	/*设置错误级别*/
+	error_reporting(E_ALL);//显示全部错误
+	error_reporting(E_ALL&~E_NOTICE);//显示全部错误除了注意级别的错误
+
+
+
--- PHP/php基础/function.php
+++ PHP/php基础/function.php
@@ -16,26 +16,82 @@
 	echo $a;//4
 	echo $b;//3
 
-/*======================*/
-
-	//函数引用外部外部变量需要用global
-	$a_name="hello word";
+	/*==========函数引用外部外部变量需要用global============*/
+	$a_name = "hello word";
 	//方法一
 	function show(){
 		global $a_name;
 		echo $a_name;
 	}
+
 	show(); //hello word
-
-
 	//方法二
 	function show1(){
 		echo $GLOBALS['a_name'];
 	}
+
 	show1();//hello word
 
+	/*==========静态变量===========*/
+
+	function foo(){
+		static $a = 0;    //静态声明,赋值必须在一行
+		echo $a++;
+	}
+
+	foo();//0
+	foo();//1
+	foo();//2
 
 
+	/*===========可变函数==============*/
 
+	function get_apple($num){
+		echo 'give I apple I need '.$num;
+	}
+	function get_banna($num){
+		echo 'give I banna I need '.$num;
+	}
+	function get_fruit($name,$num){
+		$opt = 'get_'.$name;
+		return $opt($num);
+	}
+	get_fruit('apple',5);//give I apple I need 5
 
+	/*===========嵌套函数==============*/
+	/**
+	 * 当外部函数被调用时,内部函数就会自动进入全局域,成为新的函数
+	 */
+	function out(){
+		function int(){
+			echo '当外部函数没有被调用时候,我并不存在';
+		}
+	}
+	out();
+	int();
+	/*===========闭包==============*/
+	$foo = function(){
+		echo "hello word";
+	};//记得加分号
 
+	$foo();
+
+	/*=====加载文件==========*/
+	//1.加载单个文件
+	set_include_path('src');//设置路径
+	require_once ('test1.php');//设置文件名
+	require_once ('test2.php');
+	require_once ('test3.php');
+	test2();
+	test3();
+
+	//2.加载多个文件
+	set_include_path(get_include_path().PATH_SEPARATOR.'dir1');
+	//get_include_path() 获取已加载路径,PATH_SEPARATOR  linux上是一个":"号,WIN上是一个";"号
+	set_include_path(get_include_path().PATH_SEPARATOR.'dir2');
+	require_once ('test1.php');//设置文件名
+	require_once ('test2.php');
+	require_once ('test3.php');
+	require_once ('test4.php');
+	test3();
+	test4();
